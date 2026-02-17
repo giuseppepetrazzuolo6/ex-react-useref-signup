b6e41beee6dac7575ed250c8478f4bc449b0bf98
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2026-02-17 15:13:42 +0100

    Milestone 2
    The previous commit concerned milestone 1.

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import { useState } from "react"
 
 function App() {
   const [nomeCompleto, setNomeCompleto] = useState("")
-  const [surname, setSurname] = useState("")
+  const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
   const [specializzazione, setSpecializzazione] = useState("default")
   const [anniEsperienza, setAnniEsperienza] = useState(0)
   const [descrizione, setDescrizione] = useState("")
 
+  const letters = "abcdefghijklmnopqrstuvwxyz";
+  const numbers = "0123456789";
+  const symbols = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/`~";
+
   const handleSubmit = (e) => {
     e.preventDefault()
     if (nomeCompleto.trim() === '' ||
-      surname.trim() === '' ||
+      username.trim() === '' ||
       password.trim() === '' ||
       specializzazione.trim() === 'default' ||
       anniEsperienza <= 0 ||
@@ -21,13 +25,43 @@ function App() {
     }
     console.log(`
       nome: ${nomeCompleto}
-      cognome: ${surname}
+      cognome: ${username}
       password: ${password}
-      specializzazione: ${nomeCompleto}
+      specializzazione: ${specializzazione}
       anni di Esperienza: ${anniEsperienza}
       descrizione: ${descrizione}
       `)
   }
+
+  const isUsernameValid =
+    username.length >= 6 &&
+    [...username].every(c =>
+      letters.includes(c.toLowerCase()) ||
+      numbers.includes(c)
+    )
+
+  const hasLetter = [...password].some(c =>
+    letters.includes(c.toLowerCase())
+  )
+  const hasNumber = [...password].some(c =>
+    numbers.includes(c)
+  )
+  const hasSymbol = [...password].some(c =>
+    symbols.includes(c)
+  )
+
+  const isPasswordValid =
+    password.length >= 8 &&
+    hasLetter &&
+    hasNumber &&
+    hasSymbol
+
+  const descrizioneTrim = descrizione.trim()
+  const isDescrizioneValid =
+    descrizioneTrim.length >= 100 &&
+    descrizioneTrim.length <= 1000
+
+
   return (
     <>
       <section>
@@ -35,19 +69,27 @@ function App() {
         <form onSubmit={handleSubmit}>
           <div>
             <input type="text"
-              placeholder="inserisci il tuo nome"
+              placeholder="Nome Completo"
               value={nomeCompleto}
               onChange={e => setNomeCompleto(e.target.value)} />
+          </div>
+          <div>
             <input type="text"
-              placeholder="Inserisci il tuo cognome"
-              value={surname}
-              onChange={e => setSurname(e.target.value)} />
+              placeholder="Username"
+              value={username}
+              onChange={e => setUsername(e.target.value)} />
+            <strong style={{ color: isUsernameValid ? "green" : "red" }}>
+              {isUsernameValid ? "Username valido" : "Solo lettere o numeri (min 6)"}
+            </strong>
           </div>
           <div>
             <input type="password"
-              placeholder="password"
+              placeholder="Password"
               value={password}
               onChange={e => setPassword(e.target.value)} />
+            <strong style={{ color: isPasswordValid ? "green" : "red" }}>
+              {isPasswordValid ? "Username valido" : "Almeno 8 caratteri"}
+            </strong>
           </div>
           <div>
             <select value={specializzazione} onChange={e => setSpecializzazione(e.target.value)}>
@@ -59,15 +101,19 @@ function App() {
           </div>
           <div>
             <input type="number"
-              placeholder="Inserisci un numero"
               value={anniEsperienza}
               onChange={e => setAnniEsperienza(e.target.value)} />
           </div>
           <div>
             <textarea type="text"
-              placeholder=""
+              placeholder="Descrizione..."
               value={descrizione}
               onChange={e => setDescrizione(e.target.value)} />
+            <strong style={{ color: isDescrizioneValid ? "green" : "red" }}>
+              {isDescrizioneValid
+                ? "Descrizione valida"
+                : "Tra 100 e 1000 caratteri"}
+            </strong>
           </div>
           <div>
             <button>Invia Form</button>
